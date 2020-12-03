cdf5d53e8a9c55d54a531c370dd738b48c507497
Ian Panlilio <ipanlilio7@gmail.com>
2020-12-02 19:35:25 -0800

    cleaned up server.js

--- server.js
+++ server.js
@@ -21,9 +21,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-// app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "public","index.html"));
-//   });
-
-
 app.listen(PORT);
